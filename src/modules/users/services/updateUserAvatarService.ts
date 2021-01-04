97f234b3cf7ea5e20d46cb0c1d50d447963dcf67
joao vitor <joao3klb@hotmail.com>
2021-01-03 21:03:25 -0300

    strat2

--- src/modules/users/services/updateUserAvatarService.ts
+++ src/modules/users/services/updateUserAvatarService.ts
@@ -6,7 +6,6 @@ import uploadConfig from '@config/upload';
 import User from '../infra/typeorm/entities/User';
 
 import IUserRepository from '../repositories/IUsersRepository';
-import IStorageProvider from '@shared/container/providers/StorageProvider/models/IStorageProvider';
 
 interface Request{
   avatar_id:string;
@@ -17,9 +16,6 @@ class updateUserAvatarService{
   constructor(
     @inject('UsersRepository')
     private usersRepository: IUserRepository,
-
-    @inject('StorageProvider')
-    private storageProvider: IStorageProvider,
     ){
 
   }
@@ -31,12 +27,9 @@ class updateUserAvatarService{
       throw new AppError('Only authenticated users can change avatar .', 401);
     }
     if(user.avatar){
-      await this.storageProvider.deleteFile(user.avatar);
     }
 
-    const filename = await this.storageProvider.saveFile(avatarFilename);
 
-    user.avatar = filename;
 
     await this.usersRepository.save(user);
 
--- src/shared/container/index.ts
+++ src/shared/container/index.ts
@@ -3,8 +3,6 @@
 import '@modules/users/providers';
 import './providers';
 
-import IAppointmentRepository from '@modules/appointments/repositories/IAppintmentRepository';
-import AppointmentRepository from '@modules/appointments/infra/typeorm/repositories/AppointmentsRepository';
 
 import IUserRepository from '@modules/users/repositories/IUsersRepository';
 import UsersRepository from '@modules/users/infra/typeorm/repositories/UserRepository';
@@ -13,11 +11,7 @@ import UsersRepository from '@modules/users/infra/typeorm/repositories/UserRepos
 import IUserTokenRepository from '@modules/users/repositories/IUserTokenReposory';
 import UserTokenRepository from '@modules/users/infra/typeorm/repositories/UserTokenRepository';
 
-import INotificationRepository from '@modules/notifications/repositories/INotificationsRepository';
-import NotificationsRepository  from '@modules/notifications/infra/typeorm/repositories/NotificationRepository';
 
 
- container.registerSingleton<IAppointmentRepository>('AppointmentRepository', AppointmentRepository);
  container.registerSingleton<IUserRepository>('UsersRepository', UsersRepository);
  container.registerSingleton<IUserTokenRepository>('UserTokensRepository',UserTokenRepository);
- container.registerSingleton<INotificationRepository>('NotificationsRepository',NotificationsRepository);
